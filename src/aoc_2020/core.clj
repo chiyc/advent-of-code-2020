eb87698e1269b8ec5c2b49083406e5bfdd145fd3
chi <cychoi@zoho.com>
2020-12-02 05:43:30 -0500

    Complete Day 2

--- src/aoc_2020/core.clj
+++ src/aoc_2020/core.clj
@@ -7,4 +7,5 @@
   (println "Day 1 - Part 1:" (day1/part1)) ; 1006176
   (println "Day 1 - Part 2:" (day1/part2)) ; 199132160
   (println "Day 2 - Part 1:" (day2/part1)) ; 603
+  (println "Day 2 - Part 2:" (day2/part2)) ; 404
   )
--- src/aoc_2020/day2/core.clj
+++ src/aoc_2020/day2/core.clj
@@ -9,7 +9,7 @@
        (filter #(= c %))
        (count)))
 
-(defn password-valid? [record]
+(defn policy1-password-valid? [record]
   (let [c        (nth record 2)
         min      (edn/read-string (nth record 0))
         max      (edn/read-string (nth record 1))
@@ -18,11 +18,30 @@
     (and (>= occurs min)
          (<= occurs max))))
 
+(defn occurs-once? [c idx1 idx2 password]
+  (let [c1 (subs password idx1 (inc idx1))
+        c2 (subs password idx2 (inc idx2))]
+    (and (not= c1 c2)
+         (or (= c c1)
+             (= c c2)))))
+
+(defn policy2-password-valid? [record]
+  (let [c        (nth record 2)
+        pos1     (edn/read-string (nth record 0))
+        pos2     (edn/read-string (nth record 1))
+        password (nth record 3)]
+    (occurs-once? c (dec pos1) (dec pos2) password)))
+
 (defn read-input1 []
   (->> (read-file "./src/aoc_2020/day2/input1.txt")
        (map #(str/split % #"[- :]+"))))
 
 (defn part1 []
   (->> (read-input1)
-       (filter #(password-valid? %))
+       (filter #(policy1-password-valid? %))
+       (count)))
+
+(defn part2 []
+  (->> (read-input1)
+       (filter #(policy2-password-valid? %))
        (count)))
